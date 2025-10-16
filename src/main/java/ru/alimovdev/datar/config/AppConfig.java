26b69021027e9eb826191535893c8082c196cf24
Dmitriy <112590331+Dmitriy-Al@users.noreply.github.com>
2025-10-16 17:16:40 +0300

    Update AppConfig.java

--- src/main/java/ru/alimovdev/datar/config/AppConfig.java
+++ src/main/java/ru/alimovdev/datar/config/AppConfig.java
@@ -3,8 +3,8 @@ package ru.alimovdev.datar.config;
 public class AppConfig {
 
     public final static String adminUser = ""; // профессия специалиста, дающая права администрирования
-    public final static String botUsername = "datarapp_bot"; // имя бота
-    public final static String botToken = "8238514107:AAEhvjnlwUaqewnolrw1cEkvZ_z4QY7ZUFU";  // токен бота
+    public final static String botUsername = "datar"; // имя бота
+    public final static String botToken = "8rw1cEkvZ_z4QY7ZUFU";  // токен бота
 
 
 }
